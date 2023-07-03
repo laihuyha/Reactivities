2cd8c70d51ae381ec47f8513b36d4d48e5a18a25
Lại Huy Hà <laihuyha@gmail.com>
2023-07-03 14:58:03 +0700

    commit

--- ui-app/src/features/activities/dashboard/ActivitiesDashBoard.tsx
+++ ui-app/src/features/activities/dashboard/ActivitiesDashBoard.tsx
@@ -1,54 +1,37 @@
-import { Column, ColumnEditorOptions, ColumnEvent } from "primereact/column";
-import { DataTable } from "primereact/datatable";
-import { InputText } from "primereact/inputtext";
 import Activity from "../../../app/models/activity";
-import ModelToColumns from "../../../utils/helper";
+import { Splitter, SplitterPanel } from "primereact/splitter";
+import ActivitiesList from "./ActivitiesList";
 
 interface Props {
   activities: Activity[];
 }
 
 const ActivitiesDashBoard = ({ activities }: Props) => {
-  const columns = ModelToColumns(activities[0], ["venue"]);
-  const cellEditor = (options: ColumnEditorOptions) => {
-    return (
-      <InputText
-        type="text"
-        value={options.value}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          options.editorCallback!(e.target.value)
-        }
-      />
+  const mappedActivities = new Map<number, Activity>();
+  if (activities.length > 0) {
+    activities.map((x) =>
+      mappedActivities.set(Number(x.title.substring(14)), x)
     );
-  };
+  }
 
-  const onCellEditComplete = (e: ColumnEvent) => {
-    let { rowData, newValue, field, originalEvent: event } = e;
-    if (newValue.trim().length > 0) rowData[field] = newValue;
-    else event.preventDefault();
-  };
+  const sortedArr = [...mappedActivities].sort((a, b) => a[0] - b[0]);
+  const sortedActivities = sortedArr.map((x) => x[1]);
   return (
     <>
-      <DataTable
-        value={activities}
-        removableSort
-        paginator
-        rows={5}
-        rowsPerPageOptions={[1, 5, 10, 25, 50]}
-        editMode="cell"
-        size="small"
-      >
-        {columns?.map(({ field, header }, index) => (
-          <Column
-            key={index}
-            sortable
-            field={field}
-            header={header}
-            editor={(options) => cellEditor(options)}
-            onCellEditComplete={onCellEditComplete}
-          ></Column>
-        ))}
-      </DataTable>
+      <Splitter style={{ height: "90vh" }}>
+        <SplitterPanel
+          size={65}
+          className="flex align-items-center justify-content-center"
+        >
+          <ActivitiesList activities={sortedActivities} />
+        </SplitterPanel>
+        <SplitterPanel
+          size={35}
+          className="flex align-items-center justify-content-center"
+        >
+          Panel 2
+        </SplitterPanel>
+      </Splitter>
     </>
   );
 };
---
+++ ui-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,17 @@
+import { Card } from "primereact/card";
+const ActivityDetails = () => {
+  return (
+    <div className="card">
+      <Card title="Title">
+        <p className="m-0">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore
+          sed consequuntur error repudiandae numquam deserunt quisquam repellat
+          libero asperiores earum nam nobis, culpa ratione quam perferendis
+          esse, cupiditate neque quas!
+        </p>
+      </Card>
+    </div>
+  );
+};
+
+export default ActivityDetails;
