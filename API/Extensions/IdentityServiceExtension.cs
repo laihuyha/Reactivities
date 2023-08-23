6a03a3e82ea26abef0eed28fef068f63049e3cda
Lại Huy Hà <Laihuyha@gmail.com>
2023-08-23 22:43:41 +0700

    ok

--- API/Extensions/IdentityServiceExtension.cs
+++ API/Extensions/IdentityServiceExtension.cs
@@ -1,9 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Domain.Entities;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence;
 
