865e4e1a3e3b1728876221903222d1ab24e2852e
Huy Hà <laihuyha@gmail.com>
2023-10-25 22:26:00 +0700

    a

--- API/Services/AutoUnlockTaskServices.cs
+++ API/Services/AutoUnlockTaskServices.cs
@@ -17,7 +17,7 @@ namespace Persistence.Sevices
     /// </summary>
     public class AutoUnlockTaskServices : IHostedService, IDisposable
     {
-        private Timer? _timerMidnight;
+        private Timer _timerMidnight;
         private readonly IServiceProvider _serviceProvider;
 
         public AutoUnlockTaskServices(IServiceProvider serviceProvider)
@@ -25,7 +25,7 @@ namespace Persistence.Sevices
             _serviceProvider = serviceProvider;
         }
 
-        public Task StartAsync(CancellationToken stoppingToken)
+        public Task StartAsync(CancellationToken cancellationToken)
         {
             // Schedule the task to run initially and repeat every day at 9:00 AM
             DateTime scheduledTime = new(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 23, 59, 0); // Example: Today at 23:59 PM
@@ -41,49 +41,26 @@ namespace Persistence.Sevices
             return Task.CompletedTask;
         }
 
-        private async void ExecuteTask(object? state)
+        private async void ExecuteTask(object state)
         {
-            using (var scope = _serviceProvider.CreateScope())
+            using var scope = _serviceProvider.CreateScope();
+            IMediator mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+            DataContext context = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+            var a = await context.Activities.AsNoTracking().ToListAsync();
+            try
             {
-                IMediator mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
-                DataContext context = scope.ServiceProvider.GetRequiredService<DataContext>();
-
-                // var queryTT = from tt in context.TaskAssignTrackings
-                //               join task in context.Tasks on tt.TaskId equals task.Id.ToString()
-                //               where string.IsNullOrEmpty(tt.EndTime)
-                //               select tt.Id.ToString();
-
-                // var queryQCT = from qct in context.QcTrackings
-                //                join task in context.Tasks on qct.TaskId equals task.Id.ToString()
-                //                where string.IsNullOrEmpty(qct.EndTime)
-                //                select qct.Idx.ToString();
-
-                // var resTT = await queryTT.ToListAsync();
-                // var resQCT = await queryQCT.ToListAsync();
-
-                try
-                {
-                    // foreach (var item in resTT)
-                    // {
-                    //     await mediator.Send(new AnnotationTaskRequest.EndTimeAnnotation { TrackingId = item, EndTime = new EndTimeDto { Percent = 0, TimeNote = "" } });
-                    // }
-
-                    // foreach (var item in resQCT)
-                    // {
-                    //     await mediator.Send(new QCResultRequest.EndTimeQc { QcTrackingId = item, EndTime = new EndTimeDto { Percent = 0, TimeNote = "" } });
-                    // }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error Message: {0}", ex.Message);
-                    Console.WriteLine($"Error Inner Message: {0}", ex.InnerException?.Message);
-                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error Message: {0}", ex.Message);
+                Console.WriteLine($"Error Inner Message: {0}", ex.InnerException?.Message);
             }
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            _timerMidnight?.Change(Timeout.Infinite, 0);
+            _ = (_timerMidnight?.Change(Timeout.Infinite, 0));
             return Task.CompletedTask;
         }
 
